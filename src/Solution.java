d470a873c6a5016311d0f3d2fa53528c4d593a82
Jeremias <Joelmata2@gmail.com>
2016-04-22 23:28:48 +0200

    first commit

---
+++ src/Solution.java
@@ -0,0 +1,13 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
+class Solution{
+
+    public static void main(String []args){ 
+        Scanner stdin = new Scanner(System.in);
+    	BigInteger bigInteger1 = new BigInteger(String.valueOf(stdin.next()));
+        BigInteger bigInteger2 = new BigInteger(String.valueOf(stdin.next()));
+        System.out.println(bigInteger1.add(bigInteger2));
+        System.out.println(bigInteger1.multiply(bigInteger2));
+	}
+}
