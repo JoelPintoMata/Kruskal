85f3d9779c30af7eea421c01c31c7780a1bba3f6
JoelPintoMata <JoelPintoMata@users.noreply.github.com>
2016-04-23 18:23:53 +0200

    Update Solution.java

--- src/Solution.java
+++ src/Solution.java
@@ -3,11 +3,11 @@ import java.util.Scanner;
 
 class Solution{
 
-    public static void main(String []args){ 
-        Scanner stdin = new Scanner(System.in);
-    	BigInteger bigInteger1 = new BigInteger(String.valueOf(stdin.next()));
-        BigInteger bigInteger2 = new BigInteger(String.valueOf(stdin.next()));
-        System.out.println(bigInteger1.add(bigInteger2));
-        System.out.println(bigInteger1.multiply(bigInteger2));
+   	public static void main(String []args){ 
+        	Scanner stdin = new Scanner(System.in);
+    		BigInteger bigInteger1 = new BigInteger(String.valueOf(stdin.next()));
+        	BigInteger bigInteger2 = new BigInteger(String.valueOf(stdin.next()));
+        	System.out.println(bigInteger1.add(bigInteger2));
+        	System.out.println(bigInteger1.multiply(bigInteger2));
 	}
 }
